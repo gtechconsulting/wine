ecb70f8e2913e79d861a182ee4e0f6620b275902
Camilo Oscar Girardelli Baptista <girardellibaptista@gmail.com>
2024-07-02 09:38:37 -0300

    creating apis inside controllers

--- src/main/java/com/camilo/wine/controller/ClientController.java
+++ src/main/java/com/camilo/wine/controller/ClientController.java
@@ -1,4 +1,24 @@
 package com.camilo.wine.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/client")
 public class ClientController {
+
+    @GetMapping(value = "/clientes-fieis")
+    public ResponseEntity<String> getShopping() {
+        return new ResponseEntity<>("Lista de clientes fieis", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/recomendacao/cliente/tipo")
+    public ResponseEntity<String> getRecomendation() {
+        return new ResponseEntity<>("Recomendacao de vinho", HttpStatus.OK);
+    }
 }
